6523282dbe8e9917f73fc0c1eb97bd268b85784a
pezhan <pezhan@codeaurora.org>
2016-01-18 10:51:10 +0800

    Email: The contact name displays as some messy codes in email attachment after sharing the contact via Email.
    When send email by smtp, the attachment's name is not encoding.
    
    Encode this attachment's name.
    
    Change-Id: I87f6f0843463230da58f79ada34a205138686453
    CRs-Fixed: 960959

--- emailcommon/src/com/android/emailcommon/internet/Rfc822Output.java
+++ emailcommon/src/com/android/emailcommon/internet/Rfc822Output.java
@@ -213,16 +213,17 @@ public class Rfc822Output {
      */
     private static void writeOneAttachment(Context context, Writer writer, OutputStream out,
             Attachment attachment) throws IOException, MessagingException {
-        writeHeader(writer, "Content-Type",
-                attachment.mMimeType + ";\n name=\"" + attachment.mFileName + "\"");
+        writeHeader(writer, "Content-Type", attachment.mMimeType + ";\n name=\""
+                + MimeUtility.foldAndEncode2(attachment.mFileName, "Content-Type".length() + 2)
+                + "\"");
         writeHeader(writer, "Content-Transfer-Encoding", "base64");
         // Most attachments (real files) will send Content-Disposition.  The suppression option
         // is used when sending calendar invites.
         if ((attachment.mFlags & Attachment.FLAG_ICS_ALTERNATIVE_PART) == 0) {
-            writeHeader(writer, "Content-Disposition",
-                    "attachment;"
-                    + "\n filename=\"" + attachment.mFileName + "\";"
-                    + "\n size=" + Long.toString(attachment.mSize));
+            writeHeader(writer, "Content-Disposition", "attachment;" + "\n filename=\""
+                    + MimeUtility.foldAndEncode2(attachment.mFileName,
+                    "Content-Disposition".length() + 2)
+                    + "\";" + "\n size=" + Long.toString(attachment.mSize));
         }
         if (attachment.mContentId != null) {
             writeHeader(writer, "Content-ID", attachment.mContentId);
